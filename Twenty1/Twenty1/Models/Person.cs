7b6c75a038d0fc7a6ee2b7145e6c5702f3ca9223
Jasmichele <jasmichele@yahoo.com>
2016-11-20 11:09:21 -0500

    Help

---
+++ Twenty1/Twenty1/Models/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Twenty1.Models
+{
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public List<Card> Hand { get; set; }
+
+        public int HandValue()
+        {
+            int returnValue = 0;
+
+            foreach (Card c in Hand)
+            {
+
+                switch (c.value.ToLower())
+                {
+                    case "ace":
+                        returnValue = returnValue + 1;
+                        break;
+
+                    case "king":
+                    case "queen":
+                    case "jack":
+                        returnValue = returnValue + 10;
+                        break;
+                    default:
+                        int num;
+                        int.TryParse(c.value, out num);
+                        break;
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
+
+   
